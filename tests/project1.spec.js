76b09f03766a91955ceead8ee0cb899a2a78ad0a
Deepak Singh <tdeepak2406@gmail.com>
2026-05-09 12:37:15 +0530

    cart validation

--- tests/project1.spec.js
+++ tests/project1.spec.js
@@ -2,38 +2,38 @@ import { test, expect } from "@playwright/test";
 
 let emails = "tdeepakxyz@gmail.com";
 let pwds = "Deepak@1234";
-test("signup page", async ({ page }) => {
-  await page.goto("https://rahulshettyacademy.com/client/#/auth/register");
-  await expect(page).toHaveTitle("Let's Shop");
+// test.skip("signup page", async ({ page }) => {
+//   await page.goto("https://rahulshettyacademy.com/client/#/auth/register");
+//   await expect(page).toHaveTitle("Let's Shop");
 
-  const name = page.locator("#firstName");
-  const lName = page.locator("#lastName");
-  const email = page.locator("#userEmail");
-  const phone = page.locator("#userMobile");
-  const occupation = page.locator("[formcontrolname='occupation']");
-  const gender = page.locator("[formcontrolname='gender'][value='Male']");
+//   const name = page.locator("#firstName");
+//   const lName = page.locator("#lastName");
+//   const email = page.locator("#userEmail");
+//   const phone = page.locator("#userMobile");
+//   const occupation = page.locator("[formcontrolname='occupation']");
+//   const gender = page.locator("[formcontrolname='gender'][value='Male']");
 
-  const pwd = page.locator("#userPassword");
-  const cnfPwd = page.locator("#confirmPassword");
-  const terms = page.locator("[type='checkbox']");
-  const submitbtn = page.locator("#login");
+//   const pwd = page.locator("#userPassword");
+//   const cnfPwd = page.locator("#confirmPassword");
+//   const terms = page.locator("[type='checkbox']");
+//   const submitbtn = page.locator("#login");
 
-  const sucess = page.locator(".headcolor");
+//   const sucess = page.locator(".headcolor");
 
-  await name.fill("Deepak");
-  await lName.fill("Singh");
-  await email.fill(emails);
-  await phone.fill("9981410004");
-  await occupation.selectOption("2: Student");
-  await gender.click();
-  await pwd.fill(pwds);
-  await cnfPwd.fill(pwds);
-  await terms.click();
-  await submitbtn.click();
-  await expect(sucess).toHaveText("Account Created Successfully");
-});
+//   await name.fill("Deepak");
+//   await lName.fill("Singh");
+//   await email.fill(emails);
+//   await phone.fill("9981410004");
+//   await occupation.selectOption("2: Student");
+//   await gender.click();
+//   await pwd.fill(pwds);
+//   await cnfPwd.fill(pwds);
+//   await terms.click();
+//   await submitbtn.click();
+//   await expect(sucess).toHaveText("Account Created Successfully");
+// });
 
-test.only("login", async ({ page }) => {
+test.beforeEach("login", async ({ page }) => {
   await page.goto("https://rahulshettyacademy.com/client/#/auth/login");
   const email = page.locator("#userEmail");
   await email.fill(emails);
@@ -42,4 +42,26 @@ test.only("login", async ({ page }) => {
   const login = page.locator("#login");
   await login.click();
   await expect(page).toHaveURL(/dashboard/);
+});
+
+test("add product in cart", async ({ page }) => {
+  const productname = "ZARA COAT 3";
+  const products = page.locator(".card");
+  let productCount = await products.count();
+  for (let i = 0; i < productCount; i++) {
+    const productName = await products.locator("b").nth(i).textContent();
+    if (productName == productname) {
+      await products.nth(i).locator("text=' Add To Cart'").click();
+      break;
+    }
+  }
+
+  await page.locator(':text-is("Cart")').click();
+
+  await page.locator("div ul").nth(0).waitFor();
+
+  const isProductPresent = await page
+    .locator("h3:has-text('ZARA COAT 3')")
+    .isVisible();
+  expect(isProductPresent).toBeTruthy();
 });
